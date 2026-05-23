fedcec77da3647a39ee32513d29d94a957166059
Tatyanka <tatyana.kutsenko.84@mail.ru>
2026-05-23 09:31:57 +0300

    Добавила решение задания №1

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import com.sun.source.util.SourcePositions;
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int rublesPerMile = 20;
+        int miles = ticketPrice / rublesPerMile;
+        System.out.println(miles);
+
+
+    }
+}
